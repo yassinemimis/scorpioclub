0bc705482a4bd63f759209f7b1011aa2e32da2bf
yassinemimis <yassinekherbouche88@gmail.com>
2025-04-28 06:04:42 +0200

    version 1.41

--- js/custom.js
+++ js/custom.js
@@ -168,3 +168,19 @@ form1.addEventListener('submit', e => {
     },
     detectRetina: true
   });
+
+let mybutton = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
